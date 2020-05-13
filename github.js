086224d27475bfcd79c5514b118dc22c6f54550c
renancvr <renancvr20@gmail.com>
2020-05-12 21:26:12 -0300

    Update github.js

--- github.js
+++ github.js
@@ -4,8 +4,8 @@ inputElement = document.querySelector('#app input');
 
 function searchRepos(){
     var user = inputElement.value;
-    url = 'https://api.github/users'+user+'/repos';
+    url = 'https://api.github.com/users/'+user+'/repos';
     console.log(url);
 }
 
-btnElement.onclick = searchRepos();
+btnElement.onclick = searchRepos;
