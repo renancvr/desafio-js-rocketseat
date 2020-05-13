0038941fbce411bb542aeb3061688bcaa23f174a
renancvr <renancvr20@gmail.com>
2020-05-12 21:57:16 -0300

    Update github.js

--- github.js
+++ github.js
@@ -5,7 +5,28 @@ inputElement = document.querySelector('#app input');
 function searchRepos(){
     var user = inputElement.value;
     url = 'https://api.github.com/users/'+user+'/repos';
-    console.log(url);
+    axios.get(url)
+        .then(function(response){
+            repos = [];
+            for(repo of response.data){
+                repos.push(repo.name)
+            }
+            render(repos);
+        })
+        .catch(function(error){
+            console.warn(error);
+        })
 }
 
 btnElement.onclick = searchRepos;
+
+function render(repos){
+
+    listElement.innerHTML = '';
+    for(repo of repos){
+        repoElement = document.createElement('li');
+        textElement = document.createTextNode(repo);
+        repoElement.appendChild(textElement);
+        listElement.appendChild(repoElement);
+    }
+}
